8541389dbfaae82938cdc97f2c14f9b260ab4d72
Ramazan Polat <ramazanpolat@gmail.com>
2020-07-12 16:07:31 +0300

    Upload to PyPi with new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='prodict',
     packages=['prodict'],  # this must be the same as the name above
-    version='0.8.9',
+    version='0.8.10',
     description='Prodict = Pro Dictionary with IDE friendly(auto code completion), dot-accessible attributes and more.',
     long_description='Ever wanted to use a dict like a class and access keys as attributes? '
                      'Prodict does exactly this.'
@@ -12,7 +12,7 @@ setup(
     author='Ramazan Polat',
     author_email='ramazanpolat@gmail.com',
     url='https://github.com/ramazanpolat/prodict',
-    download_url='https://pypi.python.org/pypi/prodict/0.8.9',
+    download_url='https://pypi.python.org/pypi/prodict/0.8.10',
     keywords=['prodict', 'python3', 'typehinting', 'dynamic-props', 'dict', 'dictionary', 'auto-complete',
               'auto-code-complete'],
     classifiers=['Development Status :: 4 - Beta',
